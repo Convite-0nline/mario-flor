12c94cec51d279dac711fc428c4ac217c5c2e2c3
Tarsilacloset <tarsilaminishopping@gmail.com>
2026-03-19 12:01:00 -0300

    tentando arrumar

--- js/momentos.js
+++ js/momentos.js
@@ -26,5 +26,21 @@ function moveSlider(direction){
         sliderList.appendChild(sliderItems[0])
         thumbnail.appendChild(thumbnailItems[0])
         slider.classList.add('next')
+    }else{
+        sliderList.appendChild(sliderItems[sliderItems.length -1])
+
+        thumbnail.appendChild(thumbnailItems[thumbnailItems.length -1])
+        slider.classList.add('prev')
     }
+
+slider.addEventListener('animationend',function(){ 
+    
+        let sliderItems = sliderList.querySelectorAll('.item')
+        let thumbnailItems = document.querySelectorAll('.thumbnail .item')
+        if(direction === 'next'){
+            slider.classList.remove('next')
+        }else{
+            slider.classList.remove('prev')
+        } {once: true} // remove the event listener after
+    })
 }
